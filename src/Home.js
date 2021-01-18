f2613701affbbfa275707981ed466fd8a467af2c
saqibsaud <saqibsaudc@gmail.com>
2021-01-18 09:16:59 +0530

    Add: using state

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,26 @@
+import { useState } from 'react';
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log('Hello, ninjas', e);
+    const [name, setName] = useState('Saqib');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('Saud');
+        setAge(30)
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('Hello ' + name, e.target);
-    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log('Hello ' + name, e.target);
+    // }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <h3>{name} is {age} years old.</h3>
             <button onClick={handleClick}>Click here</button>
-            <button onClick={(e) => handleClickAgain('Saud', e)}>Click here again</button>
+            
+            {/* <button onClick={(e) => handleClickAgain('Saud', e)}>Click here again</button> */}
         </div>
     );
 }
