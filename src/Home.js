66a04124f82c45146d40cee0ca3f9415d940fd1c
saqibsaud <saqibsaudc@gmail.com>
2021-01-18 08:27:40 +0530

    Add: Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click here</button>
+            <button onClick={(e) => handleClickAgain('Saud', e)}>Click here again</button>
         </div>
     );
 }
