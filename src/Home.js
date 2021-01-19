2cc02d629c62daa44c08b2abc8f036716e2bb370
saqibsaud <saqibsaudc@gmail.com>
2021-01-19 15:58:56 +0530

    Add: Handling error using catch function

--- src/Home.js
+++ src/Home.js
@@ -5,22 +5,32 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if(!res.ok){
+                        throw Error("Could not fetch the data from that resource");
+                    }
                     return res.json()
                 })
                 .then(data => {
                     setBlogs(data);
                     setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
                 });
         }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {error && <p>{error}</p>}
             { isPending && <p>Loading...</p>}
             { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
         </div>
