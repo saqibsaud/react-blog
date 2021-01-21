d56e3bb03e51939b6a02f4dc8f4e98043c8131e1
saqibsaud <saqibsaudc@gmail.com>
2021-01-21 16:48:06 +0530

    Add: Personal logo for blog

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
+import Logo from './assets/images/logo.png';
 
 const Navbar = () => {
     return (
         <div className="navbar">
+            <Link to="/">
+                <img src={Logo} alt=""/>
+            </Link>
             <h1>The Saqib Blog</h1>
             <div className="links">
                 <Link to="/">Home</Link>
